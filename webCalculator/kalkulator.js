a5d49fff27dfe77f440e4ba9cd3a327d5c48e6ea
william sanjaya <35284667+arifsudibyo@users.noreply.github.com>
2021-06-29 09:57:12 +0700

    update kalkulator script

--- webCalculator/kalkulator.js
+++ webCalculator/kalkulator.js
@@ -1,8 +1,102 @@
 // Introduction and Messages
-console.log("Selamat Anda berhasil menggunakan JavaScript pada Website")
-console.log("Jika kalian menyukai website ini, silahkan Subscribe ke Azam Mujahid Productions")
 alert("Welcome to my Calculator");
 
+// Calculator Function
+const calculator = {
+    displayNumber: '0',
+    operator: null,
+    firstNumber: null,
+    waitingForSecondNumber: false
+}
+
+function updateDisplay() {
+    document.querySelector("#displayNumber").innerText = calculator.displayNumber;
+ }
+  
+ function clearCalculator() {
+    calculator.displayNumber = '0';
+    calculator.operator = null;
+    calculator.firstNumber = null;
+    calculator.waitingForSecondNumber = false;
+}
+ 
+function inputDigit(digit) {
+    if (calculator.displayNumber === '0') {
+        calculator.displayNumber = digit;
+    } else {
+        calculator.displayNumber += digit;
+    }
+}
+
+function inverseNumber() {
+    if (calculator.displayNumber === '0') {
+        return;
+    }
+    calculator.displayNumber = calculator.displayNumber * -1;
+}
+
+function handleOperator(operator) {
+    if (!calculator.waitingForSecondNumber) {
+        calculator.operator = operator;
+        calculator.waitingForSecondNumber = true;
+        calculator.firstNumber = calculator.displayNumber;
+
+        calculator.displayNumber = '0';
+    } else {
+        alert('Your calculator ran into a problem and needs to restart.')
+    }
+}
+
+function performCalculation() {
+    if (calculator.firstNumber == null || calculator.operator == null) {
+        alert("Yooooooooo, no more errrrrooooooooorrrrrrrrr");
+        return;
+    }
+
+    let result = 0;
+    if (calculator.operator === "+") {
+        result = parseInt(calculator.firstNumber) + parseInt(calculator.displayNumber);
+    } else {
+        result = parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber);
+    }
+    
+    calculator.displayNumber = result;
+}
+
+// Calculator Event
+
+const buttons = document.querySelectorAll(".button");
+for (let button of buttons) {
+    button.addEventListener('click', function (event) {
+        const target = event.target;
+
+        if (target.classList.contains('clear')) {
+            clearCalculator();
+            updateDisplay();
+            return;
+        }
+
+        if (target.classList.contains('negative')) {
+            inverseNumber();
+            updateDisplay();
+            return;
+        }
+
+        if (target.classList.contains('equals')) {
+            performCalculation();
+            updateDisplay();
+            return;
+        }
+
+        if(target.classList.contains('operator')) {
+            handleOperator(target.innerText);
+            return;
+        }
+            
+        inputDigit(target.innerText);
+        updateDisplay();
+    });
+}
 /*
 Paper Mario: The Origami King[a] is a 2020 cross-genre video game released exclusively for the Nintendo Switch console. Developed by Intelligent Systems and published by Nintendo, it is the sixth game in the Paper Mario series, part of the larger Mario franchise. The story follows Mario and his friends as he sets out on a journey to prevent the Mushroom Kingdom from being transformed into origami. To do so, Mario must free Princess Peach's castle from five decorative streamers that extend across the kingdom.
 The Origami King features cross-genre gameplay, blending elements of action-adventure, role-playing (RPG), and puzzle games. Controlling Mario, the player explores a large overworld and fights enemies in a turn-based style that uses a ring-based puzzle system. In combat, enemies are scattered on a circle stylized like a dartboard separated into four rings and additional columns. The player can rotate the rings horizontally and vertically to organize the enemies into patterns that result in being able to clear them quicker.
